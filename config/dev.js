13ba1baf68b51a9fa55694144188fb820a620bca
BaiHuaYang <3481653@gmail.com>
2021-05-19 14:02:07 +0800

    init commit

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    NODE_ENV: '"development"'
+  },
+  defineConstants: {
+    REACT_APP_BASE_API: '"https://www.baidu.com"',
+  },
+  mini: {},
+  h5: {
+    esnextModules: ['taro-ui']
+  }
+}
